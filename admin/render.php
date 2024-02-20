a5e93914ce12bc05217dd4b1f4a35d7138d9a296
IngageDigitalAgency <100788067+IngageDigitalAgency@users.noreply.github.com>
2024-02-20 17:20:02 -0500

    Initialize php-packagemanger-quickbooks

---
+++ admin/render.php
@@ -0,0 +1,29 @@
+<?php
+
+session_start();
+ob_start();
+$st = microtime();
+
+require_once("config.php");
+require_once("classes/globals.php");
+require_once("classes/mailer.php");
+require_once("classes/common.php");
+require_once("classes/smtp.php");
+require_once("classes/Forms.php");
+require_once("classes/HtmlElement.php");
+require_once("classes/mptt.php");
+require_once("classes/Snoopy.php");
+//require_once("classes/tcpdf/tcpdf.php");//updating to latest version using php packagemanger ,so it will load automatically
+require_once("classes/KJV.php");
+require_once("frontend/Frontend.php");
+
+date_default_timezone_set(TZ);
+setlocale(LC_MONETARY,CURRENCY);
+$fe = new Frontend(true);
+echo $fe->render(true);
+$fe = null;
+$et = microtime();
+//echo sprintf('<!-- render time is %f seconds -->',$et - $st);
+ob_end_flush();
+
+?>
---
+++ js/finalize.js
@@ -0,0 +1,59 @@
+function cleanDatePicker() {
+	if ($.datepicker != null) {
+		var old_fn = $.datepicker._updateDatepicker;
+		$.datepicker._updateDatepicker = function(inst) {
+			old_fn.call(this, inst);
+			var buttonPane = $(this).datepicker("widget").find(".ui-datepicker-buttonpane");
+			$("<button type='button' class='ui-datepicker-clean ui-state-default ui-priority-primary ui-corner-all'>Clear</button>").appendTo(buttonPane).click(function(ev) {
+				$.datepicker._clearDate(inst.input);
+			});
+		}
+	}
+}
+
+function initDateFields() {
+	if ($.datepicker != null) {
+		$('.def_field_datepicker').datepicker({
+			showButtonPanel:true,
+			changeMonth: true,
+			changeYear:true,
+			beforeShow: function() {
+				$('#ui-datepicker-div').maxZIndex(); 
+			}
+		});
+		$('.def_field_datetimepicker').datepicker({
+			showButtonPanel:true,
+			changeMonth: true,
+			changeYear:true
+		});
+	}
+}
+
+initDateFields();
+removeDependants();
+
+step2Check = function() {
+	$.ajax({
+		url: "/ajax/render",
+		data: {t_id:32},
+		success: function( obj, status, xhr ) {
+			try {
+				obj = JSON.parse(obj);
+				$("#to-step-2").replaceWith(obj.html);
+			}
+			catch(err) {
+			}
+		}
+	});
+}
+
+togglePackages = function(el) {
+	$("table.toggle").toggleClass("hidden");
+	$("#my-cart .fa-stack-1x").toggleClass("fa-minus fa-plus");
+}
+
+scrollToItem = function(el) {
+	$('html, body').animate({
+		scrollTop: $(el).offset().top
+	}, 1000);
+}
